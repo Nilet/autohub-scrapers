e15629139356c57857b1751c75cd7ab05762917d
Matheus da Costa Granato <matheusgranato@alunos.unicesumar.edu.br>
2023-10-27 15:44:16 -0300

    Replace olx.py

--- olx/olx.py
+++ olx/olx.py
@@ -2,17 +2,23 @@ from selenium import webdriver
 from time import sleep
 from bs4 import BeautifulSoup
 import os
+import pymongo
 
+# Função para conectar ao MongoDB
+def conectar_mongodb():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["nome_do_seu_banco_de_dados"]
+    collection = db["nome_da_sua_colecao"]
+    return collection
 
+# Função para ler o arquivo
 def lerArquivo(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, filename)
-
     return open(file_path).read().strip().replace(' ', '%20').split('\n')
 
-
-def OLX_buscaVeiculo(veiculo, driver):
-
+# Função para buscar veículos na OLX e armazenar no MongoDB
+def OLX_buscaVeiculo(veiculo, driver, collection):
     driver.get(
         f"https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-pr?q={veiculo}")
 
@@ -52,9 +58,23 @@ def OLX_buscaVeiculo(veiculo, driver):
                 continue
 
         img = link.find('source')['srcset']
-        print(img)
-        veiculos.append(dict(veiculo=produto, url=url, price=price))
+
+        veiculos.append(dict(veiculo=produto, url=url, price=price, veiculoImg=img))
 
     for fusca in veiculos:
         print(fusca)
+        # Insira os dados no MongoDB
+        inserir_dados_mongodb(collection, fusca["veiculo"], fusca["url"], fusca["price"], fusca["veiculoImg"])
     print()
+
+# Função principal
+def main():
+    veiculos = lerArquivo("./veiculos.txt")
+    collection = conectar_mongodb()
+    driver = webdriver.Chrome()  # Substitua pelo seu driver
+    for veiculo in veiculos:
+        OLX_buscaVeiculo(veiculo, driver, collection)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
